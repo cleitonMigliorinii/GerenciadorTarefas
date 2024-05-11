e291fe6c52480c69ad248f6cf49ccd0deb4d5101
Gabriel Arantd <grantd.ctt@gmail.com>
2024-05-11 14:59:53 -0300

    Update 11/05/2024

---
+++ src/models/usuario/data/entity/usuario.ts
@@ -0,0 +1,12 @@
+export interface Usuario {
+    RA: string;
+    nomeUsuario: string;
+    senhaUsuario: String;
+    emailUsuario: String;
+    telefoneUsuario: String;
+    tipoUsuario: String;
+    turmaUsuario: String;
+    dataRegistroUsuario: Date;
+    situacaoUsuario: Boolean;
+    dataAlteracaoUsuario: Date;
+}
