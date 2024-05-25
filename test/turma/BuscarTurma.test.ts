c48d11a79f96aedca2b77e2f8422d1f55345a59e
Gabriel Moretti Woicolesco <114514587+GabMoretti@users.noreply.github.com>
2024-05-25 11:12:36 -0300

    buscaTurma criado

---
+++ test/turma/BuscarTurma.test.ts
@@ -0,0 +1,45 @@
+
+import { BuscarCNPJUseCase } from "../../src/models/ies/domain/useCases/BuscarCNPJUseCase";
+import { TurmaCriacaoDto } from "../../src/models/turma/data/entity/Turma";
+import { TurmaRepository } from "../../src/models/turma/data/repository/turmaRepository";
+import { BuscaTurmaUseCase } from "../../src/models/turma/domain/useCases/BuscarTurmaUseCase";
+import { CadastrarTurmaUseCase } from "../../src/models/turma/domain/useCases/CadastrarTurmaUseCase";
+import { FakeDataService } from "../../src/service/iesFake.data.service";
+
+describe("Busca de IES", () => {
+
+    let buscarTurmaUseCase : BuscaTurmaUseCase;
+    let cadastrarTurmaUseCase: CadastrarTurmaUseCase; 
+    let fakeService: any;
+
+    beforeEach( ()=>{
+        const turmaRepository = new TurmaRepository();
+        buscarTurmaUseCase = new BuscaTurmaUseCase(turmaRepository)
+        cadastrarTurmaUseCase = new CadastrarTurmaUseCase(turmaRepository)
+        fakeService = FakeDataService();
+    })
+
+    it('Buscar ies por codigo', async () => {
+
+        const turmaCriacaoDto: TurmaCriacaoDto = {
+            nome: fakeService.username,
+            dataFinalPeriodo: fakeService.dataCriacao,
+            dataInicioPeriodo: fakeService.dataCriacao,
+            codigoIes:'555'
+        }
+        const turma = await cadastrarTurmaUseCase.execute(turmaCriacaoDto);
+
+        const turmaBusca = await buscarTurmaUseCase.execute(turma.codigo);
+
+        expect(turmaBusca).toBeDefined();
+        expect(turma.codigo).toBe(turmaBusca!.codigo)
+    })
+
+    it('verificar TURMA não encontrada', async () => {
+
+        const codigo: string = '999'
+        const turmaBusca = await buscarTurmaUseCase.execute(codigo);
+
+        expect(turmaBusca).toBeNull();
+    })
+})
