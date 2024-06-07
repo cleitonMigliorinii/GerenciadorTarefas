5e1683ec9499137789496c9c56842b8e215e97d3
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2024-06-07 17:50:52 -0300

    Ajuste ies

--- prisma/migrations/20240607203128_teste/migration.sql
+++ prisma/migrations/20240607203128_teste/migration.sql
@@ -2,7 +2,11 @@
 CREATE TABLE "Ies" (
     "codigo" TEXT NOT NULL,
     "nome" TEXT NOT NULL,
+    "cnpj" TEXT NOT NULL,
     "dataCriacao" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Ies_pkey" PRIMARY KEY ("codigo")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Ies_cnpj_key" ON "Ies"("cnpj");
--- src/config/server.ts
+++ src/config/server.ts
@@ -1,16 +1,23 @@
 import fastify from "fastify";
 import { iesRoutes } from "../models/ies/routes/iesRoutes";
+import cors from '@fastify/cors'
 
 const server = fastify()
 const PORT = 3333;
 
-server.register(iesRoutes)
+server.register(iesRoutes,)
+
+
+server.register(cors, {
+    origin: '*',
+});
+
 
 server.get('/', (request, reply) => {
-    return { message : 'Você está na API da CESUL' }
+    return { message: 'Você está na API da CESUL' }
 })
 
-server.listen({port: PORT}).then(() =>{
+server.listen({ port: PORT }).then(() => {
     console.log("Servidor está rodando na porta " + PORT)
 })
 
