eaae047c0fba19b5a794f8732c98b526ec4904ea
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2024-06-08 08:12:57 -0300

    Update server.ts

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,17 +1,10 @@
 import fastify from "fastify";
 import { iesRoutes } from "../models/ies/routes/iesRoutes";
-import cors from '@fastify/cors'
 
 const server = fastify()
 const PORT = 3333;
 
 server.register(iesRoutes,)
-
-
-server.register(cors, {
-    origin: '*',
-});
-
 
 server.get('/', (request, reply) => {
     return { message: 'Você está na API da CESUL' }
