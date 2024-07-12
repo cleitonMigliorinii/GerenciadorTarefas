ae1d2e825dd1913b7218db161a87a5d84f007453
Eduardo Felipe Righes <dudu_righespb@hotmail.com>
2024-07-11 21:07:04 -0300

    ajustes

--- test/disciplinaAluno/AlterarDisciplinaAluno.test.ts
+++ test/disciplinaAluno/AlterarDisciplinaAluno.test.ts
@@ -1,39 +1,68 @@
 
+import { DisciplinaRepository } from "../../src/models/disciplina/data/repository/DisciplinaRepository";
+import { SalvarDisciplinaUseCase } from "../../src/models/disciplina/domain/useCases/SalvarUseCase";
 import { DisciplinaAlunoCriacaoDto, DisciplinaAlunoUpdateDto } from "../../src/models/disciplinaAluno/data/entity/disciplinaAluno";
 import { DisciplinaAlunoRepository } from "../../src/models/disciplinaAluno/data/repository/disciplinaAlunoRepository";
 import { AlterarDisciplinaAlunoUseCase } from "../../src/models/disciplinaAluno/domain/useCase/AlterarDisciplinaAlunoUseCase";
 import { SalvarDisciplinaAlunoUseCase } from "../../src/models/disciplinaAluno/domain/useCase/SalvarDisciplinaAlunoUseCase";
-import { AlterarIesUseCase } from "../../src/models/ies/domain/useCases/AlterarIesUseCase";
-import { SalvarIesUseCase } from "../../src/models/ies/domain/useCases/SalvarIesUseCase";
+import { UsuarioRepository } from "../../src/models/usuario/data/repository/UsuarioRepository";
+import { SalvarUsuarioUseCase } from "../../src/models/usuario/domain/useCases/SalvarUsuarioUseCase";
 import { FakeDataService } from "../../src/services/fake.data.service";
 
-describe("AlteracaoDisciplinaAlunoTest", () =>{
+describe("AlteracaoDisciplinaAlunoTest", () => {
 
-    let alterarDisciplinaAlunoUseCase : AlterarDisciplinaAlunoUseCase;
-    let salvarDisciplinaAlunoUseCase: SalvarDisciplinaAlunoUseCase; 
+    let alterarDisciplinaAlunoUseCase: AlterarDisciplinaAlunoUseCase;
+    let salvarDisciplinaAlunoUseCase: SalvarDisciplinaAlunoUseCase;
+    let salvarAlunoUseCase: SalvarUsuarioUseCase;
+    let salvarDisciplinaUseCase: SalvarDisciplinaUseCase;
     let fakeService: any;
 
-    beforeEach( ()=>{
+    beforeEach(() => {
         const disciplinaAlunoRepository = new DisciplinaAlunoRepository();
         alterarDisciplinaAlunoUseCase = new AlterarDisciplinaAlunoUseCase(disciplinaAlunoRepository)
         salvarDisciplinaAlunoUseCase = new SalvarDisciplinaAlunoUseCase(disciplinaAlunoRepository)
+        const usuarioRepository = new UsuarioRepository();
+        salvarAlunoUseCase = new SalvarUsuarioUseCase(usuarioRepository);
+        const disciplinaRepository = new DisciplinaRepository();
+        salvarDisciplinaUseCase = new SalvarDisciplinaUseCase(disciplinaRepository);
         fakeService = FakeDataService();
     })
 
     it('Alterar vinculo aluno -> disciplina cadastrado', async () => {
 
+        const alunoCriacaoDto = {
+            RA: fakeService.RA,
+            nomeUsuario: fakeService.nome,
+            senhaUsuario: fakeService.senha,
+            emailUsuario: fakeService.email,
+            telefoneUsuario: fakeService.telefone,
+            tipoUsuario: fakeService.tipoUser,
+            turmaID: fakeService.turma,
+            situacaoUsuario: fakeService.situacao
+        }
+
+        const aluno = await salvarAlunoUseCase.execute(alunoCriacaoDto);
+
+        const disciplinaCriacaoDto = {
+            nome: fakeService.nome,
+            professor: fakeService.nome,
+            coordenador: fakeService.nome
+        }
+
+        const disciplina = await salvarDisciplinaUseCase.execute(disciplinaCriacaoDto);
+
         const disciplinaAlunoCriacaoDto: DisciplinaAlunoCriacaoDto = {
-            codigoAluno: fakeService.codigo,
-            codigoDisciplina: fakeService.codigo,
+            codigoAluno: aluno.RA,
+            codigoDisciplina: disciplina.codigo,
             situacao: 'A'
         }
         const disciplinaAluno = await salvarDisciplinaAlunoUseCase.execute(disciplinaAlunoCriacaoDto);
 
-        const disciplinaAlunoAlterarDto : DisciplinaAlunoUpdateDto = {
+        const disciplinaAlunoAlterarDto: DisciplinaAlunoUpdateDto = {
             situacao: "Inativo"
         }
 
-        const disciplinaAlunoUpdate = 
+        const disciplinaAlunoUpdate =
             await alterarDisciplinaAlunoUseCase.execute(disciplinaAluno.codigo, disciplinaAlunoAlterarDto);
 
         expect(disciplinaAlunoUpdate).toBeDefined()
--- test/disciplinaAluno/SalvarDisciplinaAluno.test.ts
+++ test/disciplinaAluno/SalvarDisciplinaAluno.test.ts
@@ -1,6 +1,10 @@
+import { DisciplinaRepository } from "../../src/models/disciplina/data/repository/DisciplinaRepository";
+import { SalvarDisciplinaUseCase } from "../../src/models/disciplina/domain/useCases/SalvarUseCase";
 import { DisciplinaAlunoCriacaoDto } from "../../src/models/disciplinaAluno/data/entity/disciplinaAluno";
 import { DisciplinaAlunoRepository } from "../../src/models/disciplinaAluno/data/repository/disciplinaAlunoRepository";
 import { SalvarDisciplinaAlunoUseCase } from "../../src/models/disciplinaAluno/domain/useCase/SalvarDisciplinaAlunoUseCase";
+import { UsuarioRepository } from "../../src/models/usuario/data/repository/UsuarioRepository";
+import { SalvarUsuarioUseCase } from "../../src/models/usuario/domain/useCases/SalvarUsuarioUseCase";
 import { FakeDataService } from "../../src/services/fake.data.service";
 
 
@@ -8,20 +12,47 @@ import { FakeDataService } from "../../src/services/fake.data.service";
 describe('SalvarDisciplinaAluno', () => {
 
     let salvarDisciplinaAlunoUseCase: SalvarDisciplinaAlunoUseCase;
+    let salvarAlunoUseCase: SalvarUsuarioUseCase;
+    let salvarDisciplinaUseCase: SalvarDisciplinaUseCase;
     let fakeService: any;
 
     beforeEach(() => {
         //Sempre antes do teste vai rodar esse bloco
         const disciplinaAlunoRepository = new DisciplinaAlunoRepository();
         salvarDisciplinaAlunoUseCase = new SalvarDisciplinaAlunoUseCase(disciplinaAlunoRepository);
+        const usuarioRepository = new UsuarioRepository();
+        salvarAlunoUseCase = new SalvarUsuarioUseCase(usuarioRepository);
+        const disciplinaRepository = new DisciplinaRepository();
+        salvarDisciplinaUseCase = new SalvarDisciplinaUseCase(disciplinaRepository);
         fakeService = FakeDataService();
     })
 
     it('teste de vinculação de aluno com nova disciplina', async () => {
 
+        const alunoCriacaoDto = {
+            RA: fakeService.RA,
+            nomeUsuario: fakeService.nome,
+            senhaUsuario: fakeService.senha,
+            emailUsuario: fakeService.email,
+            telefoneUsuario: fakeService.telefone,
+            tipoUsuario: fakeService.tipoUser,
+            turmaID: fakeService.turma,
+            situacaoUsuario: fakeService.situacao
+        }
+
+        const aluno = await salvarAlunoUseCase.execute(alunoCriacaoDto);
+
+        const disciplinaCriacaoDto = {
+            nome: fakeService.nome,
+            professor: fakeService.nome,
+            coordenador: fakeService.nome
+        }
+
+        const disciplina = await salvarDisciplinaUseCase.execute(disciplinaCriacaoDto);
+
         const disciplinaAlunoCriacaoDto: DisciplinaAlunoCriacaoDto = {
-            codigoAluno: fakeService.codigo,
-            codigoDisciplina: fakeService.codigo,
+            codigoAluno: aluno.RA,
+            codigoDisciplina: disciplina.codigo,
             situacao: 'A'
         }
 
